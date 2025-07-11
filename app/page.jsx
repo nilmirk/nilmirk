f0aa1652380ce5687458fca9e923f7e326841329
nilmirk <nilmirk.partner@gmail.com>
2025-07-11 17:59:09 +0400

    modified:   app/page.jsx 	modified:   components/client.jsx 	modified:   styles/blocks.scss delete videos

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,7 @@
-import { VideoList } from "@/components/client";
-
 export default function Page({ children }) {
   return (
     <div className="page v-64 w-100">
       <div className="v-32 w-100">
-        <h1 className="h1">Видео</h1>
-        <VideoList />
       </div>
     </div>
   );
